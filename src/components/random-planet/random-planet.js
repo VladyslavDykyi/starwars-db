520764f8519644ea39cd3bb8db4856bc687269cd
Vladyslav Dykyi <vladislavdykyi33@gmail.com>
2022-01-13 20:50:09 +0200

    добавил обработку ошибки fetch( проблема с поиском или когда апи недоступно ...) закончил главу работа с сервером,

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -4,6 +4,7 @@ import SwapiService from "../../services/swapi-service";
 
 import './random-planet.css';
 import Spinner from "../spinner";
+import ErrorIndicator from "../error-indicator/error-indicator";
 
 export default class RandomPlanet extends Component {
 
@@ -23,25 +24,39 @@ export default class RandomPlanet extends Component {
 		this.setState({
 			planet,
 			loading: false,
+			error: false,
 		});
 	}
 
+	onError = (err) => {
+		this.setState({
+			error: true,
+			loading: false,
+		});
+	};
+
 	updatePlanet() {
 		const id = Math.floor(Math.random() * 25) + 2;
+		console.log(id);
 		this.swapiService
 			.getPlanet(id)
-			.then(this.onPlanetLoaded);
+			.then(this.onPlanetLoaded)
+			.catch(this.onError);
 	}
 
 	render() {
 
-		const {planet, loading,} = this.state;
+		const {planet, loading, error,} = this.state;
 
+		const hasData = !(loading || error);
+
+		const errorMessage = error ? <ErrorIndicator/> : null;
 		const spinner = loading ? <Spinner/> : null;
-		const content = !loading ? <PlanetView planet={planet}/> : null;
+		const content = hasData ? <PlanetView planet={planet}/> : null;
 
 		return (
 			<div className="random-planet jumbotron rounded">
+				{errorMessage}
 				{spinner}
 				{content}
 			</div>
